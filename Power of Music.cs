7bf40c69241a66dbb6bf6987e439aaa4ad799e1f
Gleb <glebtolkacev08@gmail.com>
2025-02-10 19:58:16 +0800

    начало положено (мне страшно)

---
+++ Power of Music.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slightly_cooler_sound_board
+{
+    internal class Power_of_Music
+    {
+        public static string[] GetAllAudioInputs() 
+        {
+
+            return null;
+        }
+    }
+}
