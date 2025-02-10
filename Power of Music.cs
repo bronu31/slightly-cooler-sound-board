4bd24e87082f44db76ec02c50e17a605beaa820e
Gleb <glebtolkacev08@gmail.com>
2025-02-10 20:19:11 +0800

    кажется что-то есть

--- Power of Music.cs
+++ Power of Music.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NAudio.Wave;
 namespace slightly_cooler_sound_board
 {
     internal class Power_of_Music
     {
         public static string[] GetAllAudioInputs() 
         {
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+            WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(i);
+            }
+            return null;
+        }
 
+        public static string[] GetAllAudioOutputs()
+        {
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                WaveOutCapabilities deviceInfo = WaveOut.GetCapabilities(i);
+            }
             return null;
         }
     }
